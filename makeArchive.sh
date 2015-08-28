2be6ffc2edd472597d4cf8933c5ce64beb6e4f21
Pierre Lepair <pierre.lepair@epiphyte.be>
2015-08-28 23:36:15 +0200

    makeArchive.2015-08-28_23-36-15

--- makeArchive.sh
+++ makeArchive.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+TS=$(date +"%Y-%m-%d_%H-%M-%S")
+CMD=
+
 tar -cf master.tar ./*
 gzip master.tar
 rm archive/*
 mv master.tar.gz archive
+
+git add -A
+git commit -m "makeArchive.$TS"
