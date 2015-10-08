e5d04dd6da823300e6fd1a77c70a3f761ca0ec1e
Miguel Diaz <miguel.diaz@codescrum.com>
2015-10-08 18:56:59 -0500

    apply set +e/-e when reloading spotlight

--- os/osx/defaults.sh
+++ os/osx/defaults.sh
@@ -687,12 +687,15 @@ defaults write com.apple.spotlight orderedItems -array \
     '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
     '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
     '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+
+set +e
 # Load new settings before rebuilding the index
 killall mds > /dev/null 2>&1
 # Make sure indexing is enabled for the main volume
 sudo mdutil -i on / > /dev/null
 # Rebuild the index from scratch
 sudo mdutil -E / > /dev/null
+set -e
 
 ###############################################################################
 # Terminal & iTerm 2                                                          #
