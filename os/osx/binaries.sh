62f258adb21a9f27ac308956a0c9c225820ffd07
Miguel Diaz <miguel.diaz@codescrum.com>
2015-10-08 12:15:14 -0500

    update apps.sh, do not reinstall everything every time

--- os/osx/binaries.sh
+++ os/osx/binaries.sh
@@ -15,31 +15,48 @@ if test ! $(which brew); then
 fi
 
 # Update homebrew
-brew update && brew reinstall brew-cask
+brew update
 show_status "Updating homebrew" "$?"
 
+set +e
+
+echo "Installing coreutils, findutils, bash v4 and grep (dupe)"
+
 # Install GNU core utilities (those that come with OS X are outdated)
-brew reinstall coreutils
+brew install coreutils
 show_status "Installing GNU coreutils" "$?"
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew reinstall findutils
+brew install findutils
 show_status "Installing findutils" "$?"
 
 # Install Bash 4
-brew reinstall bash
+brew install bash
 show_status "Installing Bash" "$?"
 
 # Install more recent versions of some OS X tools
 brew tap homebrew/dupes
-brew reinstall homebrew/dupes/grep
+brew install homebrew/dupes/grep
 show_status "Installing dupes' grep" "$?"
+
+set -e
+
 # Install other useful binaries
 binaries=(
-  # graphicsmagick
+  ack
+  tree                # output directory and file structure in the console
+  the_silver_searcher # 'ag' commandline tool (a better grep)
+  git                 # git
+  hub                 # git+hub (from Github)
+  fasd                # Command-line productivity booster, offers quick access to files and directories, inspired by autojump, z and v, https://github.com/clvv/fasd
+  graphicsmagick      # imagemagick alternative, provides some of the same executables
+  postgres            # Postgres
+  phantomenv          # PhantomJS version manager (rbenv for phantomjs)
+  jenv                # java version manager (rbenv for java) [javas must be manually added]
+  qt                  # qt lib
+  # imagemagick       # watch out for graphicsmagick possible conflicts with executables, http://www.graphicsmagick.org/utilities.html
   # boot2docker
   # webkit2png
-  # phantomjs
   # rename
   # zopfli
   # ffmpeg
@@ -48,18 +65,13 @@ binaries=(
   # sshfs
   # trash
   # node
-  # tree
-  # hub
-  # ack
-  # git
-  # hub
-  # fig
-  # go
 )
 
 # Install the binaries
+set +e # If already installed, non-zero status error is reported, skip that
 brew install ${binaries[@]}
 show_status "Installing brew binaries" "$?"
+set -e # revert back to errors aborting the entire script
 
 # Add osx specific command line tools
 #if test ! $(which subl); then
