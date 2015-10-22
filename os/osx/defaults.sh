fde4f0ba85541f7cfc9dcb6102a409866f1d77fc
Miguel Diaz <miguel.diaz@codescrum.com>
2015-10-22 10:42:55 -0500

    remove duplicate (and commented) application killing code

--- os/osx/defaults.sh
+++ os/osx/defaults.sh
@@ -979,14 +979,3 @@ echo "Done. Note that some of these changes require a logout/restart to take eff
 
 
 ###############################
-
-
-
-###############################################################################
-# Kill affected applications
-###############################################################################
-#for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" "Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" "Transmission" "Twitter" "iCal"; do
-#killall "${app}" > /dev/null 2>&1
-#done
-echo "Done. Note that some of these changes require a logout/restart to take effect."
-
